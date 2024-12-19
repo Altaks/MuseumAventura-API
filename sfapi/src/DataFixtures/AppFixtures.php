6326e7622f604c8c425a11450ba2bc186f2c5bd3
cpilotte <clement.pilotte@etudiant.univ-lr.fr>
2024-12-19 17:41:42 +0100

    fix: reduce lines size

--- sfapi/src/DataFixtures/AppFixtures.php
+++ sfapi/src/DataFixtures/AppFixtures.php
@@ -20,8 +20,9 @@ class AppFixtures extends Fixture
         $course = new Course();
         $course->setTitle('La malédiction du masque Gélédé')
             ->setDescription(
-                "Marguerite a besoin de vous pour lever la malédiction du masque gélédé. Des monstres attirés par le masque " .
-                "viennent voler les œuvres du musée. Parcourir le musée à la recherche des objets permettant de lever " .
+                "Marguerite a besoin de vous pour lever la malédiction du masque gélédé. Des monstres " .
+                "attirés par le masque " .
+                "viennent voler les œuvres du musée. Parcourir le musée à la recherche des objets permettant de lever ".
                 "la malédiction avant qu'il ne soit trop tard. Réunir un instrument, un poil d'animal et une statue. " .
                 "Une fois réunie, le rituel pour lever la malédiction peut se faire."
             )
@@ -72,57 +73,75 @@ class AppFixtures extends Fixture
                 'type' => PuzzleTypeEnum::GUESSIMAGE_EASY,
                 'story' => [
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?" .
+                        "usp=drive_link',
                         'text' => 'Nous voilà dans la galerie de zoologie !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?" .
+                        "usp=drive_link',
                         'text' => 'Je ne sais malheuresement pas à quoi ressemble l\'animal que nous recherchons...',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?" .
+                        "usp=drive_link',
                         'text' => 'En revanche, j\'ai une plume appartenant à ce dernier.',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?" .
+                        "usp=drive_link',
                         'text' => 'Sauras-tu retrouver à quel animal il appartient ?',
                     ],
                 ],
                 'activity' => [
-                    'ref_image' => 'https://drive.google.com/file/d/1G1Dg4p3Z60GhitkGEnon0flZjpivFhYI/view?usp=drive_link',
+                    'ref_image' => 'https://drive.google.com/file/d/1G1Dg4p3Z60GhitkGEnon0flZjpivFhYI/view?" .
+                        "usp=drive_link',
                     'answers' => [
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1c_y8wdyrQODC_pJaakgQ77TiZlJORBu_/view?usp=drive_link'],
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1GzwJPYTQLTiB61QepQTCpUfCfjasg4sd/view?usp=drive_link'],
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1yTo0fbWavOJQut_MejGqd4pKE7AFA_zZ/view?usp=drive_link'],
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1yywsoFJKLkbIXe2vB2XAyZUH133nSp6-/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1c_y8wdyrQODC_pJaakgQ77TiZl" .
+                        "JORBu_/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1GzwJPYTQLTiB61QepQTCpUfCfja" .
+                        "sg4sd/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1yTo0fbWavOJQut_MejGqd4pKE7" .
+                        "AFA_zZ/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1yywsoFJKLkbIXe2vB2XAyZUH1" .
+                        "33nSp6-/view?usp=drive_link'],
                     ],
                     'hints' => [
-                        ['image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link', 'text' => ' Je crois que notre animal adore prendre de la hauteur dès qu\'il peut'],
+                        ['image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link', 'text' => ' Je crois que notre animal adore prendre de la hauteur" .
+                         " dès qu\'il peut'],
                     ],
                     'solution' => 2,
                     'question' => "À quelle plume appartient cet animal ?",
                 ],
                 'success' => [
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Bravo, tu as raison !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Cette plume appartient effectivement à XXX',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
-                        'text' => 'Tâchons de ne pas oublier son nom, nous en aurons besoin pour lever la malédiction !',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
+                        'text' => 'Tâchons de ne pas oublier son nom, nous en aurons besoin pour" .
+                         " lever la malédiction !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
-                        'text' => 'Maintenant, allons chercher notre instrument ! Rendez-vous dans la salle des arts musicaux (salle 24) à l\'étage 2',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
+                        'text' => 'Maintenant, allons chercher notre instrument ! Rendez-vous dans la salle" .
+                         " des arts musicaux (salle 24) à l\'étage 2',
                     ],
                 ],
                 'failure' => [
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Mince, tu t\'es trompé, réessaie',
                     ],
                 ],
@@ -134,52 +153,67 @@ class AppFixtures extends Fixture
                 'type' => PuzzleTypeEnum::AMONGUS,
                 'story' => [
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Tous ces instruments sont vraiment impressionants !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
-                        'text' => 'Pourtant, l\'un d\'entre eux est un leurre, et n\'est qu\'une illusion lancée par le masque gélédé !',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
+                        'text' => 'Pourtant, l\'un d\'entre eux est un leurre, et n\'est qu\'une illusion" .
+                         " lancée par le masque gélédé !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Mais lequel peut-il être bien être ?',
                     ],
                 ],
                 'activity' => [
                     'answers' => [
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1dU158cFV74phSf0wobc-yNVL79kIrPdS/view?usp=drive_link'],
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1IIAEn3skLmXjwij43GH0VC1VMeucRMp0/view?usp=drive_link'],
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1lqpDO5Dsnl-DM4WiSwoQkHYu1otFUstU/view?usp=drive_link'],
-                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1z2WW4X935pymcnAkmSl6mx6ZtW4oPmft/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1dU158cFV74phSf0wobc-yN" .
+                        "VL79kIrPdS/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1IIAEn3skLmXjwij43GH0VC1" .
+                        "VMeucRMp0/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1lqpDO5Dsnl-DM4WiSwoQkHYu" .
+                        "1otFUstU/view?usp=drive_link'],
+                        ['type' => 'IMAGE', 'src' => 'https://drive.google.com/file/d/1z2WW4X935pymcnAkmSl6mx6ZtW4" .
+                        "oPmft/view?usp=drive_link'],
                     ],
                     'hints' => [
-                        ['image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link', 'text' => 'Je ne crois pas qu\'il y avait autant d\'instruments à cordes dans la salle avant !'],
+                        ['image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?" .
+                        "usp=drive_link', 'text' => 'Je ne crois pas qu\'il y avait autant d\'instruments à cordes" .
+                         "dans la salle avant !'],
                     ],
                     'solution' => 3,
                     'question' => 'Quel instrument est une illusion créée par le masque gélédé ?',
                 ],
                 'success' => [
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Bravo, tu as trouvé le bon instrument !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Ce n\'est effectivement pas un instrument musical !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'N\'oublie pas de bien examiner les autres objets de la salle !',
                     ],
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Continuons de notre quête !',
                     ],
                 ],
                 'failure' => [
                     [
-                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view?usp=drive_link',
+                        'image' => 'https://drive.google.com/file/d/12BeWqQm7wv9iKHLhM08swWayu7_Txz-7/view" .
+                        "?usp=drive_link',
                         'text' => 'Mince, tu t\'es trompé, réessaie',
                     ],
                 ],
